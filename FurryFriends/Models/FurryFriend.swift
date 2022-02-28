d1cf3de6b9221b903b1a90b690dcf3ee3e6fa0df
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-02-28 18:57:00 -0500

    I added the model view. The view takes the data given for random dog images, and applies it to the JSON object notation.

---
+++ FurryFriends/Models/FurryFriend.swift
@@ -0,0 +1,13 @@
+//
+//  FurryFriend.swift
+//  FurryFriends
+//
+//  Created by Berk Bozkurt on 2022-02-28.
+//
+
+import Foundation
+
+struct FurryFriend: Decodable, Hashable, Encodable {
+    let message: String
+    let status: String
+}
