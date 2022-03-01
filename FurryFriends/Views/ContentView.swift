2b27de3579a95d4e91aa3158d157429cc5f87faa
Berk Bozkurt <BBozkurt@lcs.on.ca>
2022-02-28 19:41:13 -0500

    Some syntax errors fixed.

--- FurryFriends/Views/ContentView.swift
+++ FurryFriends/Views/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     
     // MARK: Stored properties
+    var currentFurryFriend: FurryFriend = FurryFriend(message: "", status: "")
     
     // Address for main image
     // Starts as a transparent pixel – until an address for an animal's image is set
@@ -22,7 +23,7 @@ struct ContentView: View {
                 
                 // Shows the main image
                 RemoteImageView(fromURL: currentImage)
-               
+                
                 // Push main image to top of screen
                 Spacer()
                 
@@ -43,24 +44,30 @@ struct ContentView: View {
                     
                     Spacer()
                 }
-        // Runs once when the app is opened
-        .task {
+                
+                
+            }
             
-            // Example images for each type of pet
-            //let remoteCatImage = //"https://purr.objects-us-east-1.dream.io/i/JJiYI.jpg"
-            let remoteDogImage = "https://images.dog.ceo/breeds/labrador/lab_young.JPG"
+            // Runs once when the app is opened
+            .task {
+                
+                // Example images for each type of pet
+                //let remoteCatImage = //"https://purr.objects-us-east-1.dream.io/i/JJiYI.jpg"
+                let remoteDogImage = "https://images.dog.ceo/breeds/labrador/lab_young.JPG"
+                
+                // Replaces the transparent pixel image with an actual image of an animal
+                // Adjust according to your preference ☺️
+                currentImage = URL(string: remoteDogImage)!
+                
+            }
             
-            // Replaces the transparent pixel image with an actual image of an animal
-            // Adjust according to your preference ☺️
-            currentImage = URL(string: remoteDogImage)!
+            
+            .navigationTitle("Furry Friends")
+            
+            // MARK: Functions
             
         }
-        .navigationTitle("Furry Friends")
-        
     }
-    
-    // MARK: Functions
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
